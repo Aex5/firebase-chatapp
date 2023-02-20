f6cdd66cb6e776c2e93cd44b81365e8b6d6a8d57
Aditya <aditya@fisip.net>
2023-02-21 00:54:39 +0700

    add: domain for image

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: ["lh3.googleusercontent.com", "tuk-cdn.s3.amazonaws.com"],
   },
 };
 
